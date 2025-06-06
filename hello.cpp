b114bd395dd94c1d2b022a676228ce093e1864e8
toreerel <toreerel@gmail.com>
2025-06-06 17:35:15 +0300

    say hello

---
+++ hello.cpp
@@ -0,0 +1,16 @@
+/*
+* hello.cpp
+* print my name, location and hobby.
+* by Tore
+*/
+
+#include <cstdio>
+int main()
+{
+    //print my name
+    printf("Hello, my name is Tore.\n");
+    //print my location
+    printf("I'm from Istanbul, Turkey.\n");
+    //print my hobby
+    printf("I enjoy Gaming.\n");
+}   
